ed307df311cd0b0dbb2d898833182b2a9e6c7f49
AwrenNuit <a.rennaker@yahoo.com>
2021-02-19 19:38:02 -0600

    create basic dummy data, implement initial card layout

--- src/components/App/Cards/CardItem.js
+++ src/components/App/Cards/CardItem.js
@@ -1,7 +1,34 @@
-export default function CardItem({ label }) {
-  return(
-    <>
-      <p>Hello, {label}</p>
-    </>
+export default function CardItem({
+  availabilityPercentage,
+  isRunning,
+  oeePercentage,
+  performancePercentage,
+  qualityPercentage,
+  title,
+}) {
+  return (
+    <div className="card-wrapper">
+      <div className="title-wrapper">
+        <h3>{title}</h3>
+      </div>
+      <div className="status-wrapper">
+        <h3>{isRunning ? "Running" : "Not Running"}</h3>
+      </div>
+      <div className="percentage-wrapper">
+        <p>
+          OEE: <span>{((availabilityPercentage * performancePercentage * qualityPercentage) * 100).toFixed(2)}</span>
+        </p>
+        <p>
+          Availability: <span>{(availabilityPercentage * 100).toFixed(2)}</span>
+        </p>
+        <p>
+          Performance: <span>{(performancePercentage * 100).toFixed(2)}</span>
+        </p>
+        <p>
+          Quality: <span>{(qualityPercentage * 100).toFixed(2)}</span>
+        </p>
+      </div>
+      <div className="graph-wrapper">{/* GRAPH GOES HERE */}</div>
+    </div>
   );
 }
