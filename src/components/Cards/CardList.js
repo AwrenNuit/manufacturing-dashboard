6afee80ec71569e133ed32a8a01e93d29405b2aa
AwrenNuit <a.rennaker@yahoo.com>
2021-02-20 15:37:42 -0600

    udpate formatting

--- src/components/Cards/CardList.js
+++ src/components/Cards/CardList.js
@@ -7,26 +7,18 @@ export default function CardList() {
 
   useEffect(() => {
     axios.get(`api/oee`)
-    .then((res) => {
-      setMachineStatistics(res.data);
-    })
-    .catch((err) => {
-      console.log(`ERROR: oee GET failed: ${err}`);
-    });;
-  },[]);
+      .then((res) => setMachineStatistics(res.data))
+      .catch((err) => console.log(`ERROR: oee GET failed: ${err}`));
+  }, []);
 
-  return (
-    <>
-      {machineStatistics.map((stats, i) => (
-        <CardItem
-          availabilityPercentage={stats.availability}
-          isRunning={stats.running}
-          key={i}
-          performancePercentage={stats.performance}
-          qualityPercentage={stats.quality}
-          machine={stats.machine_number}
-        />
-      ))}
-    </>
-  );
+  return machineStatistics.map((stats, i) => (
+    <CardItem
+      availabilityPercentage={stats.availability}
+      isRunning={stats.running}
+      key={i}
+      performancePercentage={stats.performance}
+      qualityPercentage={stats.quality}
+      machine={stats.machine_number}
+    />
+  ));
 }
