8eca27b8fb99c91ffec376d74f65f87d33f93c47
AwrenNuit <a.rennaker@yahoo.com>
2021-02-20 09:55:16 -0600

    implement basic table styling

--- src/components/Cards/CardItem.js
+++ src/components/Cards/CardItem.js
@@ -17,16 +17,16 @@ export default function CardItem({
       </div>
       <div className="percentage-wrapper">
         <p>
-          OEE: <span>{((availabilityPercentage * performancePercentage * qualityPercentage) * 100).toFixed(2)}</span>
+          OEE: <span>{((availabilityPercentage * performancePercentage * qualityPercentage) * 100).toFixed(2)}%</span>
         </p>
         <p>
-          Availability: <span>{(availabilityPercentage * 100).toFixed(2)}</span>
+          Availability: <span>{(availabilityPercentage * 100).toFixed(2)}%</span>
         </p>
         <p>
-          Performance: <span>{(performancePercentage * 100).toFixed(2)}</span>
+          Performance: <span>{(performancePercentage * 100).toFixed(2)}%</span>
         </p>
         <p>
-          Quality: <span>{(qualityPercentage * 100).toFixed(2)}</span>
+          Quality: <span>{(qualityPercentage * 100).toFixed(2)}%</span>
         </p>
       </div>
       <div className="graph-wrapper">{/* GRAPH GOES HERE */}</div>
